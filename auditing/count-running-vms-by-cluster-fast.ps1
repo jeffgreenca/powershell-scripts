33b5b4cacf729442bf5ab93fe894d6153cec1ff8
jeffgreenca <jeffgreen@jpgit.com>
2015-10-12 15:14:29 -0700

    remove all

--- auditing/count-running-vms-by-cluster-fast.ps1
+++
@@ -1,8 +0,0 @@
-get-datacenter | foreach {
-  $dc = $_.Name
-  $_ | get-cluster | foreach {
-    $cluster = $_.Name
-    $count = (get-view -viewtype "VirtualMachine" -Property Name -Filter @{"Runtime.PowerState"="PoweredOn"} -SearchRoot (get-view $_).moref | Measure-Object).Count
-    write-output "$dc, $cluster, $count"
-  }
-} 
--- auditing/get-vm-resource-limits-improved.ps1
+++
@@ -1,5 +0,0 @@
-$dc = "MyDatacenter"
-$outFile = "vm-resource-limits.csv"
-
-get-view -viewtype virtualmachine -searchroot (get-datacenter $dc).id | select-object {$_.Name}, {$_.Config.Hardware.NumCPU}, {$_.Config.Hardware.NumCoresPerSocket}, {$_.Config.Hardware.MemoryMB}, {$_.Runtime.PowerState}, {$_.Guest.ToolsStatus}, {$_.Config.Version}, {$_.ResourceConfig.MemoryAllocation.Limit}, {$_.ResourceConfig.MemoryAllocation.Reservation}, {$_.ResourceConfig.MemoryAllocation.Shares.Level}, {$_.ResourceConfig.CpuAllocation.Limit}, {$_.ResourceConfig.CpuAllocation.Reservation}, {$_.ResourceConfig.CpuAllocation.Shares.Level} | export-csv $outFile
-
