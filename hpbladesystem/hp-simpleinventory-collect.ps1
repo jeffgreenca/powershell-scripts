e923a6fed3bc272305062991b22cd852b23968b2
Jeff Green <jeffgreen@jpgit.com>
2016-03-28 08:08:59 -0700

    updates

--- hpbladesystem/hp-simpleinventory-collect.ps1
+++ hpbladesystem/hp-simpleinventory-collect.ps1
@@ -4,15 +4,16 @@
 write-host "HP BladeSystem Inventory Collection Script... summarized version."
 write-host "--`n"
 
-$pw = read-host "Enter common Administrator password (warning - will be echo'd to screen)"
+$user = read-host "Enter user"
+$pw = read-host "Enter $user password (warning - will be echo'd to screen)"
 
-$list = import-csv hp-simpleinventory-oa-list.txt
+$list = import-csv 'hp-simpleinventory-oa-list - Copy.txt'
 
 foreach($oa in $list) {
 	Write-Output "Collecting info for $($oa.Location) $($oa.ip)"
 	$ip = $oa.ip
-	$matchCriteria = "(^Totals:)|(^\d.[^<]+$)|(Product Name:)|(Server Blade #)|(CPU \d:)|(Memory:)|(FLB Adapter \d:)|(Mezzanine \d:)"
-	$raw = plink -m .\hp-simpleinventory-commands.txt -pw $pw Administrator@$ip
+	$matchCriteria = "(^Totals:)|(^\d.[^<]+$)|(Product Name:)|(Server Blade #)|(CPU \d:)|(Memory:)|(FLB Adapter \d:)|(Mezzanine \d:)|(Embedded RAID)"
+	$raw = plink -m .\hp-simpleinventory-commands.txt -pw $pw $user@$ip
 	$summary = $raw | ? {$_ -match $matchCriteria}
 	
 	write-output "Summary for $($oa.Location) -- $($oa.ip)" > "hp-summary-$($oa.Location)-$ip.txt"
