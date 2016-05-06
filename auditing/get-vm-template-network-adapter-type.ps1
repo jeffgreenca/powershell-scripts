edb074eaffc21cfbe4970c7ac11389f5e2ab8476
Jeff Green <jeffgreen@jpgit.com>
2016-05-06 10:09:54 -0700

    Update get-vm-template-network-adapter-type.ps1

--- auditing/get-vm-template-network-adapter-type.ps1
+++ auditing/get-vm-template-network-adapter-type.ps1
@@ -1,4 +1,7 @@
 $vc = "vcenter"
 Connect-VIServer $vc
 
-Get-NetworkAdapter -Template * | select {$vc}, {$_.parent.name}, {$_.parent.extensiondata.guest.guestfullname}, name, type
+Get-NetworkAdapter -Template * | select {$vc}, {$_.parent.name}, {$_.parent.extensiondata.guest.guestfullname}, name, networkname, macaddress, type
+
+#Or for VMs
+#Get-NetworkAdapter -VM * | select {$vc}, {$_.parent.name}, {$_.parent.extensiondata.guest.guestfullname}, name, networkname, macaddress, type
